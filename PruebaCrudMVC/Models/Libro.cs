17cff4ee9d88eaef4c658bb04f8c8f8d558bb42c
USER <USER@DESKTOP-EATI25B>
2023-12-16 22:33:37 -0400

    Agregar archivos de proyecto.

---
+++ PruebaCrudMVC/Models/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruebaCrudMVC.Models
+{
+    public partial class Libro
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Autor { get; set; }
+    }
+}
